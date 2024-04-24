722a4e2a6c1f6e5de03449ed3488f9fe7a24349a
Katie Wessman <kjeanne@gmail.com>
2024-04-24 15:50:13 +0800

    Add spring initilzr project

---
+++ src/test/java/dev/katiejeanne/d288oa/D288OaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.katiejeanne.d288oa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D288OaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
