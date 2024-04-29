977552931ef39c4aab83f245718e81c2003481bf
Katie Wessman <kjeanne@gmail.com>
2024-04-29 18:11:05 +0800

    Part H: Add controller class w/validation from part G

--- src/main/java/dev/katiejeanne/d288oa/controllers/CheckoutController.java
+++ src/main/java/dev/katiejeanne/d288oa/controllers/CheckoutController.java
@@ -1,6 +1,28 @@
 package dev.katiejeanne.d288oa.controllers;
 
+import dev.katiejeanne.d288oa.services.CheckoutService;
+import dev.katiejeanne.d288oa.services.PurchaseData;
+import dev.katiejeanne.d288oa.services.PurchaseResponse;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/checkout")
+@Valid
 public class CheckoutController {
 
-    // Include validation
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping()
+    public PurchaseResponse placeOrder(@RequestBody @Valid PurchaseData purchaseData) {
+
+        return checkoutService.placeOrder(purchaseData);
+
+    }
+
 }
